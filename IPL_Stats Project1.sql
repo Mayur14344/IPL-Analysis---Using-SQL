429bbda3b93671dd918ff2389ee144cd7294b403
Mayur waklekar <waklekarmayur@gmail.com>
2026-01-18 20:04:21 +0530

    Add files via upload

---
+++ IPL_Stats Project1.sql
@@ -0,0 +1,133 @@
+create database ipl_stats;
+use ipl_stats;
+
+CREATE TABLE ipl (
+    team1  VARCHAR(100),
+    team2  VARCHAR(100),
+    match_date DATE,
+    toss_winner VARCHAR(100),
+    toss_decision VARCHAR(20),
+    winner VARCHAR(100),
+    player_of_match VARCHAR(100),
+    venue VARCHAR(200),
+    city VARCHAR(100),
+    team1_players TEXT,
+    team2_players TEXT,
+    season INT,
+    match_number INT,
+    match_type VARCHAR(50),
+    result VARCHAR(50),
+    result_margin decimal(5,2),
+    target_runs INT,
+    target_overs DECIMAL(4,1),
+    super_over VARCHAR(10)
+);
+
+
+
+select * from ipl;
+
+# Team Analysis :
+
+#Teams Played Total Matches
+select COUNT(*) as total_matches
+from ipl;
+
+#Most win by Each Team
+select winner, COUNT(*) AS wins
+from ipl
+where winner is not null
+group by winner
+order by wins desc;
+
+#most matches played in Each venues
+select venue, COUNT(*) as matches
+from ipl
+group by venue
+order by matches desc;
+
+
+#Highest Matches win by Team 
+select winner, COUNT(*) as total_wins
+from ipl
+group by winner
+order by total_wins desc
+limit 1;
+
+#highest total runs scored by a team
+select winner as team, MAX(target_runs) as highest_runs
+from ipl
+group by winner
+order by highest_runs desc
+limit 1;
+
+#Matches Won Batting First vs field first
+SELECT toss_decision, COUNT(*) AS wins
+FROM ipl
+WHERE winner = toss_winner
+GROUP BY toss_decision;
+
+
+# Player Anyalsis :
+
+
+#Most Player of the Match Awards
+select player_of_match, COUNT(*) as awards
+from ipl
+group by player_of_match
+order by awards desc;
+
+#Top 5 players with most player of the match awards
+select player_of_match, COUNT(*) as awards
+from ipl
+group by player_of_match
+order by awards desc
+limit 5;
+
+#Most Player of the Match awards won in a single season by player
+select season, player_of_match, COUNT(*) AS awards
+from ipl
+group by season, player_of_match
+order by awards desc
+limit 1;
+
+#Matches played where a player won Player of the Match
+select match_number, season, venue
+from ipl
+where player_of_match = 'ms dhoni';
+
+
+#venue/team/player combine anaylsis
+
+#Most Player of the Match Awards won by Venue 
+select venue, player_of_match, COUNT(*) as awards
+from ipl
+group by venue, player_of_match;
+
+#Match Victories per Team at Each Stadium
+select venue, winner, COUNT(*) as wins
+from ipl
+group by venue, winner
+order by wins desc;
+
+#super over 
+select match_number, season, team1, team2, winner
+from ipl
+where super_over = 'N';
+
+#CTE
+#pom(player of match)
+
+#Top 10 players with most player of the match awards
+with pom_count as (
+    select
+        player_of_match,
+        COUNT(*) as awards
+    from ipl
+    where player_of_match is not null
+    group by  player_of_match
+)
+select *
+from pom_count
+order by awards desc
+limit 10;
